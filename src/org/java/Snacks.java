22ffda1fb8c7b99048392184b056d0f14dbd1d7e
andreaconv <andrea2convertini@gmail.com>
2023-09-14 12:18:35 +0200

    snack 3

--- src/org/java/Snacks.java
+++ src/org/java/Snacks.java
@@ -24,24 +24,40 @@ public class Snacks {
 		//Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, 
 		//e da queste vuole mostrare a video una falsa lista di invitati con nome e cognome.
 		
-		String[] nomi = {
-				"Giovanni", "Marco", "Antonio", "Luca", "Michele", "Alessio"
-		};
-		
-		String[] cognomi = {
-				"Rossi", "Bianchi", "Russo", "Esposito", "Martini", "Ferrari",
-		};
-		
-		String[] invitati = new String[nomi.length];
-		
-		Random rnd = new Random();
-		
-		for(int i=0; i<nomi.length; i++) {
-			invitati[i] = nomi[rnd.nextInt(nomi.length - 1)] + " " + cognomi[rnd.nextInt(cognomi.length - 1)];
-			System.out.println(invitati[i]);
-		}
+//		String[] nomi = {
+//				"Giovanni", "Marco", "Antonio", "Luca", "Michele", "Alessio"
+//		};
+//		
+//		String[] cognomi = {
+//				"Rossi", "Bianchi", "Russo", "Esposito", "Martini", "Ferrari",
+//		};
+//		
+//		String[] invitati = new String[nomi.length];
+//		
+//		Random rnd = new Random();
+//		
+//		for(int i=0; i<nomi.length; i++) {
+//			invitati[i] = nomi[rnd.nextInt(nomi.length - 1)] + " " + cognomi[rnd.nextInt(cognomi.length - 1)];
+//			System.out.println(invitati[i]);
+//		}
 		//--------------------------------------------------------
 		
+		//SNAK 3
+		//Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione dispari
+		
+		int[] numeri = {1, 3, 5, 7, 12, 15, 17, 23, 28, 31, 37, 44, 47, 51, 59};
+		
+		int sum = 0;
+
+		for(int i=0; i<numeri.length; i++) {
+			
+			if(!(i % 2 == 0)) {
+				
+				sum += numeri[i];
+			}
+		}
+		System.out.println(sum);
+		//--------------------------------------------------------
 	}
 
 }
