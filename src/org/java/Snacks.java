25d568e1c8121e4c30721b9d3c69871dc1e86195
andreaconv <andrea2convertini@gmail.com>
2023-09-14 12:01:02 +0200

    snack 2

--- src/org/java/Snacks.java
+++ src/org/java/Snacks.java
@@ -1,5 +1,6 @@
 package org.java;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Snacks {
@@ -9,14 +10,37 @@ public class Snacks {
 		
 		//SNACK 1
 		//Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
-		System.out.print("Inserisci un numero: ");
-		int numInserito = sc.nextInt();
+		//System.out.print("Inserisci un numero: ");
+		//int numInserito = sc.nextInt();
 		
-		if(numInserito % 2 == 0) {
-			System.out.println("il numero inserito è pari: " + numInserito);
-		} else {
-			System.out.println("il numero successivo è: " + (numInserito + 1));			
+		//if(numInserito % 2 == 0) {
+		//	System.out.println("il numero inserito è pari: " + numInserito);
+		//} else {
+		//	System.out.println("il numero successivo è: " + (numInserito + 1));			
+		//}
+		//--------------------------------------------------------
+		
+		//SNACK 2
+		//Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, 
+		//e da queste vuole mostrare a video una falsa lista di invitati con nome e cognome.
+		
+		String[] nomi = {
+				"Giovanni", "Marco", "Antonio", "Luca", "Michele", "Alessio"
+		};
+		
+		String[] cognomi = {
+				"Rossi", "Bianchi", "Russo", "Esposito", "Martini", "Ferrari",
+		};
+		
+		String[] invitati = new String[nomi.length];
+		
+		Random rnd = new Random();
+		
+		for(int i=0; i<nomi.length; i++) {
+			invitati[i] = nomi[rnd.nextInt(nomi.length - 1)] + " " + cognomi[rnd.nextInt(cognomi.length - 1)];
+			System.out.println(invitati[i]);
 		}
+		//--------------------------------------------------------
 		
 	}
 
